5e08e4fe53bdc898c66b62dd34e89b5f1f03e20b
Juan Diego Gonzalez <jd.gonzalezg1@uniandes.edu.co>
2021-12-03 13:38:50 -0500

    Version inicial

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,9 @@
 const data = require("../assets/data");
 
 function getProducts(query) {
-  return data.filter((item) => item.name.toLowerCase().includes(query));
+  return !query
+    ? data
+    : data.filter((item) => item.name.toLowerCase().includes(query));
 }
 
 module.exports = { getProducts };
--- frontreact/src/pages/Home.js
+++ frontreact/src/pages/Home.js
@@ -1,14 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { Card } from "../components/Card";
 
 export const Home = ({ searchKey }) => {
+  const url = "http://localhost:3001/api/products";
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
   return (
-    <section id='home'>
-      <div className='home-container'>
+    <section id="home">
+      <div className="home-container">
         <h1>Gallery</h1>
-        <div className='home-card'>
-          <p>Show here product cards</p>
+        <div className="home-card">
+          {products &&
+            products.map((elem) => (
+              <Card
+                key={elem.id}
+                name={elem.name}
+                picture={elem.picture}
+                price={elem.price}
+                isActive={elem.isActive}
+              />
+            ))}
         </div>
       </div>
     </section>
